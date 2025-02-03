eb12da7b263309178601d33ee06313003250b999
shalant <doug.rosenberg@gmail.com>
2025-02-03 16:41:51 -0600

    Working on getting a list of employees on front end, more basic setup

---
+++ EmployeeManagementPortal/EmployeeManagementPortal.Client/Services/EmployeeService.cs
@@ -0,0 +1,56 @@
+﻿using EmployeeManagementPortal.Models;
+using System.Net.Http.Json;
+
+namespace EmployeeManagementPortal.Client.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient _http;
+
+        public EmployeeService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public string Message { get; set; } = "Loading employees...";
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            try
+            {
+                var result = await _http.GetFromJsonAsync<ServiceResponse<List<Employee>>>("api/employees");
+
+                if(result != null)
+                {
+                    Employees = result.Data;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<ServiceResponse<Employee>> GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeManagementPortal/EmployeeManagementPortal.Client/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementPortal.Models;
+
+namespace EmployeeManagementPortal.Client.Services
+{
+    public interface IEmployeeService
+    {
+        Task<List<Employee>> GetAllEmployees();
+        Task<ServiceResponse<Employee>> GetEmployeeById(int id);
+        Task<Employee> AddEmployee(Employee employee);
+        Task UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int id);
+    }
+}

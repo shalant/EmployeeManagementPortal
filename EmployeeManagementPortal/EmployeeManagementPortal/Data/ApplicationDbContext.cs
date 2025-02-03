e4880cb23234c145c62f84ddcd31806ccc503dd7
shalant <doug.rosenberg@gmail.com>
2025-02-03 13:25:13 -0600

    Added custom styling, migration for 2 tables, basic layout

--- EmployeeManagementPortal/EmployeeManagementPortal/Data/ApplicationDbContext.cs
+++ EmployeeManagementPortal/EmployeeManagementPortal/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using EmployeeManagementPortal.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,7 @@ namespace EmployeeManagementPortal.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<HealthPlan> HealthPlans { get; set; }
     }
 }
---
+++ EmployeeManagementPortal/EmployeeManagementPortal/Data/Migrations/20250203192425_addingEmployeesAndHealthPlans.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagementPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingEmployeesAndHealthPlans : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HealthPlans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Coverage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HealthPlans", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: true),
+                    HealthPlanId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_HealthPlans_HealthPlanId",
+                        column: x => x.HealthPlanId,
+                        principalTable: "HealthPlans",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_HealthPlanId",
+                table: "Employees",
+                column: "HealthPlanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "HealthPlans");
+        }
+    }
+}

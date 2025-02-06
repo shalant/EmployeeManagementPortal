ceed0d68a04f9f3db01ff274080da6b9ebd9d543
shalant <doug.rosenberg@gmail.com>
2025-02-06 11:20:53 -0600

    got data!

--- EmployeeManagementPortal/EmployeeManagementPortal.Client/Services/IEmployeeService.cs
+++ EmployeeManagementPortal/EmployeeManagementPortal.Client/Services/IEmployeeService.cs
@@ -5,6 +5,7 @@ namespace EmployeeManagementPortal.Client.Services
     public interface IEmployeeService
     {
         event Action EmployeeChanged;
+        List<Employee> Employees { get; set; }
         Task<List<Employee>> GetAllEmployees();
         Task<ServiceResponse<Employee>> GetEmployeeById(int id);
         Task<Employee> AddEmployee(Employee employee);
